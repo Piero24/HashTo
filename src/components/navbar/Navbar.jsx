b6e1d7484b14f9d731ba0ae4b2dd1e21b18460f8
Piero24 <andrea_pietrobon@hotmail.it>
2024-12-18 16:20:38 +0100

    logo updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
                         <div className='w-full max-w-[155rem] flex justify-between px-5 sm:px-10 py-2 sm:py-3 mx-auto text-color-3'>
                             <div className='h-8 sm:h-10'>
                                 <img 
-                                    src={'/src/assets/logo.svg'} 
-                                    alt={Logo}
+                                    src={Logo} 
+                                    alt="Logo"
                                     className="h-full coh-full object-contain"
                                     onClick={() => navigate('/')}
                                 />
